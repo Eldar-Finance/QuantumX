c289e2de79edceede6133141988228d5a881a531
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-12-05 13:54:33 -0500

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
   images: {
     domains: ["media.elrond.com", "devnet-media.elrond.com"],
   },
-};
+});
--- src/utils/constants/site.ts
+++ src/utils/constants/site.ts
@@ -3,7 +3,7 @@ export const host = process.env.NEXT_PUBLIC_HOST;
 
 // HTML metata and og tags, default values for MetaHead.tsx component
 export const defaultMetaTags = {
-  title: "QuantumX - Tools Build On MultiversX Ecosystem",
+  title: "QuantumX",
   description:
     "QuantumX is a Platform that provides a tightly integrated ecosystem of dApps, that aims to get full advantage of the capabilities of MultiversX Network.",
   image: `${host}/images/dashboard.jpg`,
