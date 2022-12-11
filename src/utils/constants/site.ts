345bf1d71d16dc0be11ff461a4c409990759574c
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-12-11 13:42:09 -0500

    update kosatas address

--- src/utils/constants/site.ts
+++ src/utils/constants/site.ts
@@ -11,8 +11,12 @@ export const defaultMetaTags = {
 };
 
 // Kostas
-export const KostasAddress =
-  "erd1wlz5dy2m766fxgmygy7e30380h8tgqqj6k7rr7jzr0q6cthw64qsp6qr4s";
+export const KostasAddress1 =
+  "erd1hqaczk7hmszly8jt2a4yd3pk6dkvffjk0efy9uz6p3r65dnpfhas6nxvyd";
+export const KostasAddress2 =
+  "erd13j00d82gs7ec665z202lh25l7tjw6lpaxwe7th4e6uwlsk3r8pgq555qzg";
+export const KostasAddress3 =
+  "erd1lnmfa5p9j6qy40kjtrf0wfq6cl056car6hyvrq5uxdcalc2gu7zsrwalel";
 //  Dinos address
 export const DinosAddress =
   "erd1s5ufsgtmzwtp6wrlwtmaqzs24t0p9evmp58p33xmukxwetl8u76sa2p9rv";
@@ -20,4 +24,10 @@ export const DinosAddress =
 export const ArmandoAddress =
   "erd1ag6nvusjhcw90ntutyzsn7gntmgx9rv8xz2qczgquy48ltaxqghqxs0rkl";
 
-export const admins = [ArmandoAddress, DinosAddress, KostasAddress];
+export const admins = [
+  ArmandoAddress,
+  DinosAddress,
+  KostasAddress1,
+  KostasAddress2,
+  KostasAddress3,
+];
