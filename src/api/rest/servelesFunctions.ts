9a4d6c8b4410302f6bc51e70cea36a0eaa66ff8e
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-12-30 10:25:20 -0500

    update default next api

--- src/api/rest/servelesFunctions.ts
+++ src/api/rest/servelesFunctions.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "https://sprightly-tarsier-537dc5.netlify.app";
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = process.env.NEXT_PUBLIC_HOST || "https://eldar.finance";
+const BASE_URL = process.env.NEXT_PUBLIC_HOST || "https://www.quantumx.network";
 
 const axiosNext = axios.create({
   baseURL: BASE_URL + "/api",
