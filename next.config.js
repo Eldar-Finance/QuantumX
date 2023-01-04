dac48ce5668eadc6d9668f5bdc274d18e52487e5
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-04 11:08:24 -0500

    .

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const withPWA = require("next-pwa")({
 
 module.exports = withPWA({
   images: {
-    domains: [
-      "media.elrond.com",
-      "devnet-media.elrond.com",
-      "localhost",
-      "https://www.quantumx.network",
-      process.env.NEXT_PUBLIC_HOST,
-    ],
+    domains: ["media.elrond.com", "devnet-media.elrond.com"],
   },
 });
