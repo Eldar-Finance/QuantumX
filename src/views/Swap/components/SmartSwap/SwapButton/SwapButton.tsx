1a02504cc68ffb315b2e3b09343b0c521ab3fc41
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-07 10:59:19 -0500

    .

--- src/views/Swap/components/SmartSwap/SwapButton/SwapButton.tsx
+++ src/views/Swap/components/SmartSwap/SwapButton/SwapButton.tsx
@@ -25,6 +25,25 @@ interface IProps extends ButtonProps {
 }
 
 const SwapButton = ({ disableButton, swapInfo, ...props }: IProps) => {
+  const swapInfo2: ISmartSwapData[] = [
+    {
+      amountReceiv: "5",
+      amountsend: "0.6",
+      smartcontract:
+        "erd1x39tc3q3nn72ecjnmcz7x0qp09kp97t080x99dgyhx7zh95j0n4szskhlv",
+      token1: "WEGLD-d7c6bb",
+      token2: "RIDE-6e4c49",
+    },
+    {
+      amountReceiv: "17",
+      amountsend: "4",
+      smartcontract:
+        "erd1qqqqqqqqqqqqqpgqq67uv84ma3cekpa55l4l68ajzhq8qm3u0n4s20ecvx",
+      token1: "USDC-8d4068",
+      token2: "LKMEX-3b7d9a",
+    },
+  ];
+
   const [sessionId, setSessionId] = useState<string>();
   const address = useAppSelector(selectUserAddress);
   const toField = useAppSelector((state) => state.smartSwap.toField);
@@ -43,7 +62,7 @@ const SwapButton = ({ disableButton, swapInfo, ...props }: IProps) => {
 
   const handleSwap = async () => {
     if (swapInfo && swapInfo.length > 0 && fromElrondToken) {
-      const dataToSend = swapInfo.map((item) => {
+      const dataToSend = swapInfo2.flatMap((item) => {
         return [
           new AddressValue(new Address(item.smartcontract)),
           BytesValue.fromUTF8("swapTokensFixedInput"),
@@ -66,7 +85,7 @@ const SwapButton = ({ disableButton, swapInfo, ...props }: IProps) => {
         token: fromElrondToken,
         val: Number(fromToken.value),
         contractAddr: contractAddr.smartSwap,
-        args: [],
+        args: dataToSend,
       });
     }
   };
