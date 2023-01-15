bf20d24b27f0c84f64cd94d9275ceb5a48992ffe
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-15 12:36:57 -0500

    fix harvest of proteo admin panel

--- src/views/Farms/constants/index.tsx
+++ src/views/Farms/constants/index.tsx
@@ -23,7 +23,7 @@ export const proteoFarms: {
     Icon: <NextImage alt="" src={proteoEgldLpImg} width={45} />,
     stakedCoin: "PROTEO-EGLD",
     wsp: proteoEgldEliteWsp,
-    token: "PROTEOEGLD",
+    token: "PROTEOEGLDLP",
     aprEndpoint: "/proteoegldapi.php",
     tokenIdentifier: toknesID.proteoEgldLp,
     getFarm:
