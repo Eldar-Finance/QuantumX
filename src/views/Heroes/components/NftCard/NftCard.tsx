35f7594098dd58f49006d9273cd9f1d5c0aefd75
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-21 18:05:09 -0500

    disabled upgarde of heroes

--- src/views/Heroes/components/NftCard/NftCard.tsx
+++ src/views/Heroes/components/NftCard/NftCard.tsx
@@ -28,7 +28,8 @@ const NftCard = ({ onSubmit, nft }: IProps) => {
         mt={5}
         fontWeight="900"
         onClick={onSubmit}
-        disabled={nft.nonce >= 303}
+        // disabled={nft.nonce >= 303}
+        disabled={true}
       >
         Upgrade
       </ActionButton>
