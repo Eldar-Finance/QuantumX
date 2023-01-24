676c2b1b769de97b135d86f7f95455e60d30054c
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-24 18:23:59 -0500

    bearlybonding on farms page

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -2,9 +2,11 @@ import { Flex } from "@chakra-ui/react";
 import MyContainer from "components/Container/Container";
 import Title from "components/Farms/Title/Title";
 import Layout from "components/Layout/Layout";
+import MyTabs from "components/MyTabs/MyTabs";
 import withElronDapp from "hoc/withElronDapp";
 import WrapperPages from "hoc/WrapperPages";
 import useGetTotalValueInFarms from "utils/hooks/useGetTotalValueInFarms";
+import BearlyBonding from "./components/BearlyBonding/BearlyBonding";
 import FarmsList from "./components/FarmsList/FarmsList";
 
 const Farms = () => {
@@ -26,7 +28,38 @@ const Farms = () => {
             subtitle="Stake Liquidity Pool (LP) tokens"
             amount={totalValueLocked}
           />
-          <FarmsList />
+          <MyTabs
+            tabListProps={{
+              overflow: "auto",
+            }}
+            tabsProps={{
+              w: "full",
+              mt: 20,
+            }}
+            tabListWarapperProps={{
+              mb: 10,
+              display: "flex",
+              alignItems: "center",
+              flexDir: "column",
+              w: "full",
+            }}
+            tabProps={{
+              w: "full",
+              display: "flex",
+              alignItems: "center",
+              flexDir: "column",
+            }}
+            tabData={[
+              {
+                tabText: "Farms",
+                tabPanel: <FarmsList />,
+              },
+              {
+                tabText: "BearlyBonding",
+                tabPanel: <BearlyBonding />,
+              },
+            ]}
+          />
         </Flex>
       </MyContainer>
     </Layout>
---
+++ src/views/Farms/components/BearlyBonding/BearlyBonding.tsx
@@ -0,0 +1,87 @@
+import { Center, Flex, Grid, Heading, Text, VStack } from "@chakra-ui/react";
+import { toknesID } from "api/net.config";
+import rareUsdcImage from "assets/logos/rare-usdc.png";
+import ActionButton from "components/ActionButton/ActionButton";
+import Card from "components/Card/Card";
+import NextImage from "components/NextImage/NextImage";
+import useGetMultipleElrondTokens from "utils/hooks/useGetMultipleElrondTokens";
+
+const BearlyBonding = () => {
+  const { tokens } = useGetMultipleElrondTokens([
+    toknesID.wegld,
+    toknesID.mex,
+    toknesID.zpay,
+    toknesID.usdc,
+    toknesID.crt,
+  ]);
+  if (!tokens) return null;
+  return (
+    <Card>
+      <Grid templateColumns={"2fr 1fr"} gap={20}>
+        <Flex flexDir={"column"} w="full">
+          <Flex w="full" justify={"space-between"} mb={10}>
+            <Flex alignItems={"center"} gap={3}>
+              <NextImage alt="" src={rareUsdcImage} height={55} width={55} />
+              <Text fontWeight={"600"}> RAREUSDCLP</Text>
+            </Flex>
+            <Flex flexDir={"column"} align="center">
+              <Text color="white.400">APR</Text>
+              <Text>0%</Text>
+            </Flex>
+          </Flex>
+          <Center flexDir={"column"} gap={6} mb={10} fontSize={"lg"} flex={1}>
+            <VStack>
+              <Text color="white.400">Total ValueLocked</Text>
+              <Text>$10.383.02</Text>
+            </VStack>
+            <VStack>
+              <Text color="white.400">Staked Balance</Text>
+              <Text>0 ($0)</Text>
+            </VStack>
+          </Center>
+          <Flex w="full" justify={"space-between"} alignItems="center">
+            <Flex flexDir={"column"} w="full">
+              <Text mb={2} color="white.400" fontSize={"14px"}>
+                Stake RAREUSDCLP
+              </Text>
+              <Flex w="full" justify={"space-between"} gap={4}>
+                <ActionButton w="full" maxW={"50%"}>
+                  Stake LP
+                </ActionButton>
+                <ActionButton w="full" maxW={"50%"}>
+                  Unstake
+                </ActionButton>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+        <VStack>
+          <Heading mb={3}>EARN</Heading>
+          <Flex flexDir={"column"} flex={1} w="full" gap={5} pb={6}>
+            {tokens.map((token) => {
+              return (
+                <Flex key={token.identifier} w="full" justify={"space-around"}>
+                  <NextImage
+                    alt=""
+                    src={token.assets.svgUrl}
+                    height={35}
+                    width={35}
+                  />
+
+                  <Text fontSize={"2xl"} fontWeight="bold">
+                    13
+                  </Text>
+                </Flex>
+              );
+            })}
+          </Flex>
+          <ActionButton w="full" maxW={"110px"}>
+            Harvest
+          </ActionButton>
+        </VStack>
+      </Grid>
+    </Card>
+  );
+};
+
+export default BearlyBonding;
