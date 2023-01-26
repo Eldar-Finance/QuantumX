42032d86f5d2a88d02598da534ab4ef68460866f
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-26 13:39:58 -0500

    change bg color to tabs

--- src/components/MyTabs/MyTabs.tsx
+++ src/components/MyTabs/MyTabs.tsx
@@ -34,7 +34,7 @@ const MyTabs = ({
     <Tabs variant={"unstyled"} isLazy {...tabsProps}>
       <Box overflow={"auto"} {...tabListWarapperProps}>
         <TabList
-          bg="black"
+          bg="black.baseDark"
           w="fit-content"
           borderRadius={"full"}
           {...tabListProps}
