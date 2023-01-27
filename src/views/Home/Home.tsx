b84608450303bfd909255ace021144340601dc8a
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-27 16:51:53 -0500

    fetch pools and farm on Home too

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,24 +8,32 @@ import WrapperPages from "hoc/WrapperPages";
 import { useEffect } from "react";
 import { fetchAllFarms } from "redux/slices/farms2/funcs";
 import { fetchGeneralInfo } from "redux/slices/proteo/funcs";
-import { useAppDispatch } from "utils/hooks/redux";
+import { selectMexPairs } from "redux/slices/userAcount/account-slice";
+import { fetchMexPairs } from "redux/slices/userAcount/funcs";
+import { useAppDispatch, useAppSelector } from "utils/hooks/redux";
 import InfoText from "./components/InfoText/InfoText";
 import MainSection from "./components/MainSection/MainSection";
 import QuantumXFeatures from "./components/QuantumXFeatures/QuantumXFeatures";
 
 const Home = () => {
   const dispatch = useAppDispatch();
+  const { data: mexPairs } = useAppSelector(selectMexPairs);
   useEffect(() => {
-    dispatch(fetchAllFarms([]));
+    dispatch(fetchMexPairs());
     dispatch(fetchGeneralInfo());
   }, [dispatch]);
+  useEffect(() => {
+    if (mexPairs.length > 0) {
+      dispatch(fetchAllFarms(mexPairs));
+    }
+  }, [dispatch, mexPairs]);
   return (
     <Layout>
       <MyContainer>
         <Box mt={{ xs: "50px", "2xl": "94px" }}>
           <MainSection />
         </Box>
-        <Box mt={{ xs: "100px", "2xl": "190px" }}>
+        <Box mt={{ xs: "100px", "2xl": "180px" }}>
           <QuantumXFeatures />
         </Box>
         <Box mt="99px" mb={"50px"}>
