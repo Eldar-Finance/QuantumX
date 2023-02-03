1d13d8959d1270fa0bc0c4d1c2778924834bfa30
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-03 16:21:03 -0500

    .

--- src/components/Farms/FarmsCard/Farms2/StakeUnstake/StakeUnstake.tsx
+++ src/components/Farms/FarmsCard/Farms2/StakeUnstake/StakeUnstake.tsx
@@ -104,7 +104,7 @@ const StakeUnstake = ({ farm, userFarmItem, isPool, isBearly }: IProps) => {
           >
             UNSTAKE
           </ActionButton>
-          {hasuserStaked && epochDiffrence !== 777 && (
+          {hasuserStaked && disableUnstake && epochDiffrence !== 777 && (
             <Text fontSize={"smaller"} mt={1}>
               {timeToUnstake} remaining to unstake with 0% penalty
             </Text>
