605b74cbdd35c9b46332327640f1e5b5584b4e78
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-13 17:08:18 -0500

    .

--- src/redux/slices/farms2/funcs.ts
+++ src/redux/slices/farms2/funcs.ts
@@ -30,7 +30,7 @@ export const fetchAllFarms = createAsyncThunk(
           totalRewardsLeft: farm.field2.toNumber(),
         };
       })
-      .filter((farm) => farm.farm.farmId <= 11);
+      .filter((farm) => farm.farm.farmId < 11);
     return {
       allFarms,
       pools: mexPairs
