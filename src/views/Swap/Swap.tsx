22778c8606cb8e9aa81d762315be09161d3cf068
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-15 12:43:16 -0500

    hide p2p

--- src/views/Swap/Swap.tsx
+++ src/views/Swap/Swap.tsx
@@ -1,14 +1,10 @@
-import { Box } from "@chakra-ui/react";
 import Card from "components/Card/Card";
 import MyContainer from "components/Container/Container";
 
 import Layout from "components/Layout/Layout";
-import MyTabs from "components/MyTabs/MyTabs";
 import withElronDapp from "hoc/withElronDapp";
 import WrapperPages from "hoc/WrapperPages";
 import SwapCard from "./components/SmartSwap/SwapCard/SwapCard";
-import SwapCard2 from "./components/SwapP2P/SwapCard/SwapCard";
-import WrapEgld from "./components/SwapP2P/WrapEgld/WrapEgld";
 
 const Swap = () => {
   return (
@@ -27,37 +23,8 @@ const Swap = () => {
           border={"1px solid"}
           borderColor={"main"}
         >
-          <MyTabs
-            tabListProps={{
-              overflow: "auto",
-            }}
-            tabsProps={{
-              w: "full",
-              display: "flex",
-              alignItems: "center",
-              flexDir: "column",
-            }}
-            tabProps={{
-              fontSize: { xs: "sm", md: "md" },
-              px: "20px",
-              fontWeight: "600",
-            }}
-            tabData={[
-              {
-                tabText: "Smart Swap",
-                tabPanel: <SwapCard />,
-              },
-              {
-                tabText: "P2P Swap",
-                tabPanel: (
-                  <Box>
-                    <WrapEgld />
-                    <SwapCard2 />{" "}
-                  </Box>
-                ),
-              },
-            ]}
-          />
+          <SwapCard />
+          {/* p2p is hidden */}
         </Card>
       </MyContainer>
     </Layout>
