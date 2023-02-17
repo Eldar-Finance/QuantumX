9c4d13e185f6be09032ab855fdb7309dfbdd72d9
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-17 15:32:21 -0500

    $ value gray on swap

--- src/views/Swap/components/SmartSwap/TextField/TextField.tsx
+++ src/views/Swap/components/SmartSwap/TextField/TextField.tsx
@@ -150,7 +150,7 @@ const TextField = ({
             />
           )}
           {dollarAmount && (
-            <Text>
+            <Text color={"grayText"}>
               ≈ ${formatNumber(preventExponetialNotation(dollarAmount))}
             </Text>
           )}
