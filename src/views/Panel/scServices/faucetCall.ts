1c5375a9c5a9ebb05ec52ead90287b3da2c481e6
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-23 17:57:26 -0500

    fix setReward on hype faucet

--- src/views/Panel/scServices/faucetCall.ts
+++ src/views/Panel/scServices/faucetCall.ts
@@ -10,7 +10,7 @@ export const deposiHype = async (amount: string | number) => {
   return res;
 };
 export const changeReward = async (amount: string | number, token: string) => {
-  const res = await scCall("hypezoneWsp", "deposit", [
+  const res = await scCall("hypezoneWsp", "setReward", [
     BytesValue.fromUTF8(token),
     new BigUIntValue(new BigNumber(0)),
     new BigUIntValue(new BigNumber(setElrondBalance(amount, 18))),
