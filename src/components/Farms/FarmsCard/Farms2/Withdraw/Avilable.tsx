e43a9ed7bb138cebb9d3b70c57db5fbee814af02
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-27 16:07:30 -0500

    incresea  bearly bonding gas limit to 180m , failed again with 150m

--- src/components/Farms/FarmsCard/Farms2/Withdraw/Avilable.tsx
+++ src/components/Farms/FarmsCard/Farms2/Withdraw/Avilable.tsx
@@ -27,7 +27,7 @@ const Avilable = ({ farm, userFarmRewards }: IProps) => {
       "farms2",
       "harvest",
       [new BigUIntValue(new BigNumber(farm.farm.farmId))],
-      150000000
+      180000000
     );
   };
   const isAFarmBoost = useIsBearFarm(farm);
--- src/views/Farms/components/BearlyBonding/BearlyCard.tsx
+++ src/views/Farms/components/BearlyBonding/BearlyCard.tsx
@@ -89,7 +89,7 @@ const BearlyCard = ({ farm, multifarmRewardsLeft }: IProps) => {
       "farms2",
       "harvest",
       [new BigUIntValue(new BigNumber(farm.farm.farmId))],
-      150000000
+      180000000
     );
   };
   const { isSrbStaker } = useCanUsePool7();
