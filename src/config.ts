354ed9aeab7961afa5059e6a02065562e12d3dcf
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-02-28 18:03:01 -0500

    .

--- src/config.ts
+++ src/config.ts
@@ -134,7 +134,7 @@ export const TOKENS_ID = {
 };
 
 //general configs
-export const walletConnectV2ProjectId = "bb4a880517d6fcc65cdb9cac0a238b4e";
+export const walletConnectV2ProjectId = "9b1a9564f91cb659ffe21b73d5c4e2d8";
 export const apiTimeout = 6000;
 export const TOOLS_API_URL = "https://tools.elrond.com";
 export const sampleAuthenticatedDomains = [TOOLS_API_URL];
