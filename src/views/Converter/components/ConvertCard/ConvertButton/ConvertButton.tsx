c4cc99d0a54d6fc841df17bffecb9270f7e694b9
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-03-03 18:08:44 -0500

    if the user has no rare then he cannot press the button and the button should say
    “You need 0.5 RARE”

--- src/views/Converter/components/ConvertCard/ConvertButton/ConvertButton.tsx
+++ src/views/Converter/components/ConvertCard/ConvertButton/ConvertButton.tsx
@@ -1,10 +1,15 @@
 import ActionButton from "components/ActionButton/ActionButton";
 import { selectConvertInfo } from "redux/slices/converter/converter-slice";
+import { formatBalance } from "utils/functions/formatBalance";
+import { formatTokenI } from "utils/functions/tokens";
 import { useAppSelector } from "utils/hooks/redux";
+import useGetUserTokens from "utils/hooks/useGetUserTokens";
+import { protocolFee, protocolFeeToken } from "views/Converter/utils/contants";
 import { convertTokens, IConvertTokenData } from "views/Converter/utils/scCall";
 
 const ConvertButton = () => {
   const swapInfo = useAppSelector(selectConvertInfo);
+  const [s, userProtocolToken] = useGetUserTokens(protocolFeeToken, true);
   const handleSubmit = () => {
     const data: IConvertTokenData[] = swapInfo.map((si) => {
       const data: IConvertTokenData = {
@@ -20,13 +25,19 @@ const ConvertButton = () => {
     convertTokens(data);
   };
 
+  const canUserPayProtocolFee =
+    userProtocolToken && formatBalance(userProtocolToken, true) >= 0.5;
+
   return (
     <ActionButton
       onClick={handleSubmit}
       mt={16}
-      disabled={swapInfo.length === 0}
+      disabled={swapInfo.length === 0 || !canUserPayProtocolFee}
+      fontWeight="600"
     >
-      Convert tokens
+      {canUserPayProtocolFee
+        ? "Convert tokens"
+        : `You need ${protocolFee} ${formatTokenI(protocolFeeToken)}`}
     </ActionButton>
   );
 };
