7aa5fe496e8ff507cbbd22414076e55ceb9781eb
Kostas Tzoumpas <kststz@gmail.com>
2023-03-26 13:29:08 +0300

    changing gas | try #2

--- src/views/MoonDustX/utils/scCall.ts
+++ src/views/MoonDustX/utils/scCall.ts
@@ -76,7 +76,7 @@ export const convertTokens = async (
         realValue: fromToken.value,
         contractAddr: contractAddr.smartSwap,
         args: dataToSend,
-        gasL: gasL || 35000000,
+        gasL: gasL || 50000000,
       });
 
       return tx;
