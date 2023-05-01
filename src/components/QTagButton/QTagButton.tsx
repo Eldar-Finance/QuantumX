821b1345e41cf2843ee398b5cc1df1d172760a4a
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-05-01 00:51:36 -0400

    Claim QxTag only shows when user is logged in

--- src/components/QTagButton/QTagButton.tsx
+++ src/components/QTagButton/QTagButton.tsx
@@ -1,4 +1,5 @@
 import { Spinner } from "@chakra-ui/react";
+import { useGetLoginInfo } from "@multiversx/sdk-dapp/hooks";
 import ActionButton from "components/ActionButton/ActionButton";
 import Link from "next/link";
 import { routeNames } from "utils/routes";
@@ -6,11 +7,16 @@ import useGetUserQTag from "views/Tags/hooks/useGetQTag";
 
 const QTagButton = () => {
   const { tagInfo, isLoading } = useGetUserQTag();
+  const { isLoggedIn } = useGetLoginInfo();
   return (
-    /* @ts-ignore */
-    <ActionButton as={Link} href={routeNames.qtags} fontWeight={"normal"}>
-      {tagInfo.tag || (isLoading ? <Spinner size={"sm"} /> : "Create QxTags")}
-    </ActionButton>
+    <>
+      {isLoggedIn && (
+        /* @ts-ignore */
+        <ActionButton as={Link} href={routeNames.qtags} fontWeight={"normal"}>
+          {tagInfo.tag || (isLoading ? <Spinner size={"sm"} /> : "Claim QxTag")}
+        </ActionButton>
+      )}
+    </>
   );
 };
 
