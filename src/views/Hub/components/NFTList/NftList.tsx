e01f59e5f77ef5ca53a077c0d2fb334f1c0a8a51
Nick Koukis <nikoskoukis99@gmail.com>
2023-05-02 20:02:21 +0300

    Add new Hub - MiceCity

--- src/views/Hub/components/NFTList/NftList.tsx
+++ src/views/Hub/components/NFTList/NftList.tsx
@@ -4,6 +4,7 @@ import { contractAddr } from "api/net.config";
 import { EGLDPayment, ESDTTransfer } from "api/sc/calls";
 import srbImage from "assets/hub/srbcatalog.jpg";
 import vacineImage from "assets/hub/vacine.png";
+import miceCityImage from "assets/hub/miceCityImage.jpg";
 import BigNumber from "bignumber.js";
 import { formatBalance } from "utils/functions/formatBalance";
 import { formatTokenI } from "utils/functions/tokens";
@@ -28,8 +29,8 @@ const NftList = () => {
     elrondToken: tokens.find((t) => t.identifier === offers[0].token),
   };
   const offer3 = {
-    ...offers[2],
-    elrondToken: tokens.find((t) => t.identifier === offers[2].token),
+    ...offers[3],
+    elrondToken: tokens.find((t) => t.identifier === offers[3].token),
   };
 
   const handleSubmitHubOffer = (offer) => {
@@ -95,9 +96,9 @@ const NftList = () => {
           </Text>
         }
       />
-      {/* <NftCard
+      <NftCard
         token={offer3.token}
-        iamge={flamieImage}
+        iamge={miceCityImage}
         onSubmit={() => handleSubmitHubOffer(offer3)}
         text={
           <Text>
@@ -110,7 +111,7 @@ const NftList = () => {
           </Text>
         }
         disabled={offer3.numberOfAvilableNfts === 0}
-      /> */}
+      />
     </Center>
   );
 };
