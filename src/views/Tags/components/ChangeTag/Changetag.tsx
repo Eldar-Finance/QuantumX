662cf0747853347d6e79ed66417f1e48ea662e23
Kostas Tzoumpas <kststz@gmail.com>
2023-05-09 19:37:53 +0300

    NFTs limit for tag extensions increased to 350

--- src/views/Tags/components/ChangeTag/Changetag.tsx
+++ src/views/Tags/components/ChangeTag/Changetag.tsx
@@ -106,7 +106,7 @@ const ChangeTag = () => {
   useEffect(() => {
     const fetchDataFromApi = async () => {
       try {
-        const apiURL = `${network.apiAddress}/accounts/${userAddress}/collections?size=50`;
+        const apiURL = `${network.apiAddress}/accounts/${userAddress}/collections?size=350`;
   
         const responseData = await fetchData(apiURL);
         const collectionsToCheck = ["QXFLM-06e81a", "QXHR-9b0bc6"];
