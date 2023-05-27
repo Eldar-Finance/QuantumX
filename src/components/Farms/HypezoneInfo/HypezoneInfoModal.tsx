6aa53f344097064872b01b39b15a093117867de8
Nick Koukis <nikoskoukis99@gmail.com>
2023-05-27 11:37:17 +0300

    Init Info Hypezone Modal

---
+++ src/components/Farms/HypezoneInfo/HypezoneInfoModal.tsx
@@ -0,0 +1,53 @@
+import MyModal from 'components/Modal/Modal';
+import { Text, ModalHeader, Divider, ModalBody, ModalCloseButton, Flex } from '@chakra-ui/react';
+import { InfoIcon } from '@chakra-ui/icons';
+const HypezoneInfoModal = ({ onClose }) => {
+
+    return (
+        <MyModal
+            isOpen={true}
+            bg="black.baseDark"
+            onClose={onClose}
+            size="xl"
+        >
+            <ModalHeader>INFO / FEES</ModalHeader>
+            <ModalCloseButton mt={2}/>
+            <Divider />
+            <ModalBody padding={10}>
+                <Flex direction="column" gap="10px">
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">Important things you need to know before accessing HYPEZone Area</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">The gas fee for any transaction in the HYPEZone is 0.5 $RARE, includes staking and claiming rewards</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">Withdrawal fees for HYPEZone Farms and Pools</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">0-14 Days - 5% Fee</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">15-28 Days - 2.5% Fee</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">Minimum staking period 28days, after 28days 0 fees</Text>
+                    </Flex>
+                    <Flex alignItems="start">
+                        <InfoIcon color="white" mr="2" mt={1} />
+                        <Text color="white" fontWeight="normal">Please note that adding LP or tokens into the Pool or Farm will automatically reset the timer to 28days</Text>
+                    </Flex>
+                </Flex>
+            </ModalBody>
+        </MyModal>
+    );
+}
+
+
+export default HypezoneInfoModal;
