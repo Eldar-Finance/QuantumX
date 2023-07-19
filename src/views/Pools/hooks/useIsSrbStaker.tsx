61e76d0d7ba3a9a2c3fe7ef618d896653c368bae
Kostas Tzoumpas <kststz@gmail.com>
2023-07-19 18:56:23 +0300

    srb pools added

--- src/views/Pools/hooks/useIsSrbStaker.tsx
+++ src/views/Pools/hooks/useIsSrbStaker.tsx
@@ -11,7 +11,7 @@ const useIsSrbStaker = () => {
   );
 
   return {
-    isSrbStaker: isOneOfTheNumbersInArray([3, 4, 5, 6, 7], data || []),
+    isSrbStaker: isOneOfTheNumbersInArray([3, 4, 5, 6, 7, 90, 129, 135], data || []),
     isLoading,
     error,
   };
