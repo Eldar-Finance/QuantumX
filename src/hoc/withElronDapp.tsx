0f7e0ff7458ef1e42ebfcf26d6c480ebc7f7668c
Nick Koukis <nikoskoukis99@gmail.com>
2023-09-08 20:20:57 +0300

    revert id

--- src/hoc/withElronDapp.tsx
+++ src/hoc/withElronDapp.tsx
@@ -43,7 +43,7 @@ const withElronDapp = (Component) => (props) => {
             environment={network.id}
             customNetworkConfig={{
               name: "quantumxConfig",
-              walletConnectV2ProjectId: "6bec64742092caa5f1b382312bb17be2", 
+              walletConnectV2ProjectId: "cf388e978587b4cba673b4080fb9d89b",
             }}
             dappConfig={{
               shouldUseWebViewProvider: true,
