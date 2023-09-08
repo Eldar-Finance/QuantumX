972454976e6a78287195e14c2a1fbf2e8b817e2c
klepi21 <kolepidas@gmail.com>
2023-09-08 19:58:22 +0300

    push

--- src/hoc/withElronDapp.tsx
+++ src/hoc/withElronDapp.tsx
@@ -46,7 +46,7 @@ const withElronDapp = (Component) => (props) => {
             environment={network.id}
             customNetworkConfig={{
               name: "quantumxConfig",
-              walletConnectV2ProjectId: "6bec64742092caa5f1b382312bb17be2",
+              walletConnectV2ProjectId: "6bec64742092caa5f1b382312bb17be2", 
             }}
             dappConfig={{
               shouldUseWebViewProvider: true,
