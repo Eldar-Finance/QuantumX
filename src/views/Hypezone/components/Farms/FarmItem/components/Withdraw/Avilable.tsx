15cc2302c902854fc6c01b8979e15708797d39c4
Kostas Tzoumpas <kststz@gmail.com>
2023-10-19 17:49:07 +0300

    increase gas

--- src/views/Hypezone/components/Farms/FarmItem/components/Withdraw/Avilable.tsx
+++ src/views/Hypezone/components/Farms/FarmItem/components/Withdraw/Avilable.tsx
@@ -35,7 +35,7 @@ const Avilable = ({ farm, userFarmRewards, disable }: IProps) => {
         "farms2",
         "harvest",
         [new BigUIntValue(new BigNumber(farm.farm.farmId))],
-        180000000
+        220000000
       );
 
       sendMultipleTransactions({ txs: [t1, t2] });
