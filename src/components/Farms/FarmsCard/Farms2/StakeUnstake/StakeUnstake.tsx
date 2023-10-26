b5470af2bba8d753b33fd71a8b623d8f345c3358
Kostas Tzoumpas <kststz@gmail.com>
2023-10-26 18:08:04 +0300

    opacity

--- src/components/Farms/FarmsCard/Farms2/StakeUnstake/StakeUnstake.tsx
+++ src/components/Farms/FarmsCard/Farms2/StakeUnstake/StakeUnstake.tsx
@@ -153,7 +153,7 @@ const StakeUnstake = ({ farm, userFarmItem, isPool, isBearly, farmUserRewards }:
             UNSTAKE
           </ActionButton>
           {shouldUserHarvestWarning && 
-            <Text color={"yellow.300"} fontSize={"16px"} position={"absolute"} top={{sm: "42px", md: "-35px"}} right={{sm: "5px", md: "75px"}} whiteSpace={"nowrap"}>
+            <Text opacity={0.7} color={"yellow.300"} fontSize={"16px"} position={"absolute"} top={{sm: "42px", md: "-35px"}} right={{sm: "5px", md: "75px"}} whiteSpace={"nowrap"}>
               ⚠️ {" "} Harvest your rewards before unstaking.
             </Text>
           }
