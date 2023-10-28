1063a6ed18c9c1fba98bce4e85f715fa0e44c321
Kostas Tzoumpas <kststz@gmail.com>
2023-10-28 17:52:10 +0300

    errors fixed - plz deployloyloyloyloyloy

--- src/views/Swap/components/SmartSwap/WrapEgld/WrapModal/WrapModal.tsx
+++ src/views/Swap/components/SmartSwap/WrapEgld/WrapModal/WrapModal.tsx
@@ -14,6 +14,7 @@ import {
 import { useGetAccountInfo } from "@multiversx/sdk-dapp/hooks/account/useGetAccountInfo";
 import { contractAddr } from "api/net.config";
 import { EGLDPayment } from "api/sc/calls";
+import { wrapEgldpWsp } from "api/sc/sc";
 import ActionButton from "components/ActionButton/ActionButton";
 import MyModal from "components/Modal/Modal";
 
@@ -43,7 +44,7 @@ const WrapModal = ({ isOpenModal, onCloseModal }) => {
   const handleSubmit = async () => {
     if (amount) {
       const res = await EGLDPayment(
-        contractAddr.wrapEgld,
+        wrapEgldpWsp,
         "wrapEgld",
         amount,
         [],
