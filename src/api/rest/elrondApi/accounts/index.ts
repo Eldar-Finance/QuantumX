08c32b902a7ab4560e65a6099a4d0ce2841ccef7
klepi21 <kolepidas@gmail.com>
2023-12-01 17:16:25 +0200

    Update index.ts

--- src/api/rest/elrondApi/accounts/index.ts
+++ src/api/rest/elrondApi/accounts/index.ts
@@ -37,7 +37,7 @@ export const getTokensByNfts = async (address, nfts) => {
 };
 
 export const getMexPairs = async () => {
-  return await axiosEldron.get("/mex/pairs?size=150");
+  return await axiosEldron.get("/mex/pairs?size=320");
 };
 
 export const getEgldBalance = async (address): Promise<IElrondUserAccount> => {
