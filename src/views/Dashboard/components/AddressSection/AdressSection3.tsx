2ff863b104bdad1e6aada50d18dea19c18924ce2
Kostas Tzoumpas <kststz@gmail.com>
2023-12-11 14:23:21 +0200

    Send in connected dropdown

--- src/views/Dashboard/components/AddressSection/AdressSection3.tsx
+++ src/views/Dashboard/components/AddressSection/AdressSection3.tsx
@@ -1,25 +1,40 @@
-import { CheckIcon, CopyIcon, Search2Icon } from "@chakra-ui/icons";
-import { Box, HStack, Icon, Link, Text, useClipboard } from "@chakra-ui/react";
+import { CheckIcon, CopyIcon, Search2Icon, ArrowUpIcon } from "@chakra-ui/icons";
+import { Box, HStack, Icon, Link, Text, useClipboard, useDisclosure } from "@chakra-ui/react";
 import { network } from "api/net.config";
 import { selectUserAddress } from "redux/slices/userAcount/account-slice";
 import { formatAddress } from "utils/functions/formatAddress";
 import { useAppSelector } from "utils/hooks/redux";
+import SendTokens from "../SendTokens/SendTokens";
+import { useState } from "react";
+import TransactionModal from "../SendTokens/TransactionModal/TransactionModal";
 
 const AddressSection3 = () => {
   const address = useAppSelector(selectUserAddress);
   const { hasCopied, onCopy } = useClipboard(address);
+
+  // const [showSendModal, setShowSendModal] = useState(false);
+  // const onSendClick = () => {
+  //   setShowSendModal(true);
+  // };
+
+  const { onClose, onOpen, isOpen } = useDisclosure();
+
   return (
     <Box
       display={"flex"}
       justifyContent={"space-between"}
-      // marginBottom={"5px"}
-      // marginLeft={"10px"}
-      // m={2}
-      // gap={3}
     >
-      {/* {formatAddress(address)} */}
 
-      <HStack  onClick={onCopy} cursor={"pointer"} gap={2} w={"full"}>
+      <HStack onClick={onOpen} cursor={"pointer"} gap={1}>
+        <Text>
+          Send
+        </Text>
+        <Icon as={ArrowUpIcon} fontSize={"18px"} mb={0.5}/>
+      </HStack>
+
+      {isOpen && <TransactionModal isOpen={true} onClose={onClose} />}
+    
+      <HStack  onClick={onCopy} cursor={"pointer"} gap={1}>
         <Text>
           Copy
         </Text>
@@ -29,23 +44,22 @@ const AddressSection3 = () => {
           <Icon as={CopyIcon} fontSize={"16px"} mb={0.5}/>
         )}
       </HStack>
-      {/* <HStack > */}
 
-        <Link
-          isExternal
-          href={`${network.explorerAddress}/accounts/${address}`}
-          aria-label="find in explorer"
-          gap={1}
-          whiteSpace={"nowrap"}
-        >
-          <HStack>
-          <Text>
-            Explorer view
-          </Text>
-          <Search2Icon fontSize={"15px"} mb={0.5}/>
-          </HStack>
-        </Link>
-      {/* </HStack> */}
+      <Link
+        isExternal
+        href={`${network.explorerAddress}/accounts/${address}`}
+        aria-label="find in explorer"
+        gap={1}
+        whiteSpace={"nowrap"}
+      >
+        <HStack>
+        <Text>
+          Explorer
+        </Text>
+        <Search2Icon fontSize={"15px"} mb={0.5}/>
+        </HStack>
+      </Link>
+
     </Box>
   );
 };
