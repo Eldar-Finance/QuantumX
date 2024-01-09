ab38b15cbb875ef3757dc0ca203a5766020f01db
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-01-09 14:39:34 +0200

    moondust updates

--- src/views/MoonDustX/components/ConvertCard/ConvertInfo/ConvertInfo.tsx
+++ src/views/MoonDustX/components/ConvertCard/ConvertInfo/ConvertInfo.tsx
@@ -1,6 +1,7 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, HStack, Text } from "@chakra-ui/react";
 import BigNumber from "bignumber.js";
 import Card from "components/Card/Card";
+import { HiServer } from "react-icons/hi2";
 import {
   selectConvertInfo,
   selectToTokenDust,
@@ -10,6 +11,7 @@ import { formatTokenI } from "utils/functions/tokens";
 import { useAppSelector } from "utils/hooks/redux";
 import useGetElrondToken from "utils/hooks/useGetElrondToken";
 import { protocolFee } from "views/MoonDustX/utils/contants";
+import { convertSlippage } from "views/MoonDustX/utils/contants";
 
 const ConvertInfo = () => {
   const toTokenToConvert = useAppSelector(selectToTokenDust);
@@ -35,7 +37,7 @@ const ConvertInfo = () => {
         mb={4}
         fontSize={{ xs: "14px", md: "md" }}
       >
-        <Text>Minimum {formatTokenI(toTokenToConvert)} to receive</Text>
+        <Text>Minimum to receive</Text>
         <Flex flexDir={"column"} alignItems="flex-end">
           <Text fontWeight="600">
             {formatBalance({
@@ -50,13 +52,23 @@ const ConvertInfo = () => {
         </Flex>
       </Flex>
       <Flex
-        justifyContent={"space-between"}
-        fontSize={{ xs: "14px", md: "md" }}
+        // justifyContent={"space-between"}
+        fontSize={{ xs: "12px", md: "14px" }}
+        gap={5}
+        mb={-4}
       >
-        <Text>Protocol Fee</Text>
-        <Text color={"GrayText"} fontWeight="600">
-          {protocolFee} %
-        </Text>
+        <HStack>
+          <Text>Fee</Text>
+          <Text color={"GrayText"} fontWeight="600">
+            {protocolFee} %
+          </Text>
+        </HStack>
+        <HStack>
+          <Text>Slippage</Text>
+          <Text color={"GrayText"} fontWeight="600">
+            {convertSlippage} %
+          </Text>
+        </HStack>
       </Flex>
     </Card>
   );
--- src/views/MoonDustX/utils/contants.ts
+++ src/views/MoonDustX/utils/contants.ts
@@ -2,5 +2,5 @@ import { toknesID } from "api/net.config";
 
 export const toTokensToConvert = [toknesID.usdc, toknesID.wegld, toknesID.rare, toknesID.wbtc,toknesID.htm];
 export const protocolFee = 0.1;
-export const convertSlippage = 3;
+export const convertSlippage = 5;
 export const limitDollarAmount = 0.1;
