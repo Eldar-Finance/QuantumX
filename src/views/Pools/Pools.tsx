66ca2ecce6b35550d3999070114bb5281d12e4e9
klepi21 <kolepidas@gmail.com>
2024-01-18 23:37:10 +0200

    check

--- src/views/Pools/Pools.tsx
+++ src/views/Pools/Pools.tsx
@@ -192,9 +192,9 @@ const Pools = () => {
           />
           <HarvestAll harvestableFarms={userHarvestableFarms} type="pools"/>
 
-          <Link marginTop={"30px"} href="https://bop.ashswap.io?ref=VzYUCcDZFt" isExternal>
+          <Link marginTop={"30px"} href="https://twitter.com/SuperRare_Bears/status/1747733437182542075?t=07ROu5JOLJC4VGynxn00Ug&s=19" isExternal>
             <Image
-              src="https://i.postimg.cc/dtHrQxks/Screenshot-2023-12-06-at-4-58-50-PM-copy.jpg"
+              src="https://i.ibb.co/3vqQJ8K/Quantum-X-Banner-01.jpg"
               alt="Description of the image content"
               style={{ borderRadius: '25px', width: '100%'}}
               height={{sm: "50px", md: "130px"}}
