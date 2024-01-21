170407af46269cce2b73513876797385159b2b51
Kostas Tzoumpas <kststz@gmail.com>
2024-01-21 19:28:03 +0200

    gas

--- src/components/Farms/FarmsCard/Farms2/Withdraw/Avilable.tsx
+++ src/components/Farms/FarmsCard/Farms2/Withdraw/Avilable.tsx
@@ -30,7 +30,7 @@ const Avilable = ({ farm, userFarmRewards }: IProps) => {
       "farms2",
       "harvest",
       [new BigUIntValue(new BigNumber(farm.farm.farmId))],
-      farm.farm.farmId === 8 ? 450000000 : 300000000
+      farm.farm.farmId === 8 ? 450000000 : 350000000
     );
   };
 
