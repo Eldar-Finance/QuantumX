ab7abb2637bdac50c1002a031d5fcc71ce07a07d
klepi21 <kolepidas@gmail.com>
2024-06-28 10:00:56 +0300

    fix bfy image

--- src/views/Badges/SftsStaking/components/BadgesCard/BadgesCard.tsx
+++ src/views/Badges/SftsStaking/components/BadgesCard/BadgesCard.tsx
@@ -77,7 +77,7 @@ const BadgesCard = () => {
               isExternal
               href="https://burnify.app/nft-burn"
             >
-              <NextImage src="https://burnify.app/static/media/Burnify_Logo_White.6101a362bf009f033832.png" alt="deadrare" width={35} />
+              <NextImage src="https://burnify.app/static/media/n_D_Flame.a473b9131dfc7a3448ca.png" width={35} />
             </Link>
             <Link
               isExternal
