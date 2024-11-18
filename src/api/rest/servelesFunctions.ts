8c099f6d46f56013a43cb8ccb6b968affae26a2e
klepi21 <kolepidas@gmail.com>
2024-11-18 11:16:27 +0200

    checl

--- src/api/rest/servelesFunctions.ts
+++ src/api/rest/servelesFunctions.ts
@@ -6,6 +6,10 @@ const BASE_URL = process.env.NEXT_PUBLIC_HOST || "https://www.quantumx.network";
 
 const axiosNext = axios.create({
   baseURL: BASE_URL + "/api",
+  headers: {
+    // Remove User-Agent if it's being set
+    // 'User-Agent': 'YourUserAgent' // This line should be removed
+  },
 });
 
 export default axiosNext;
