54c251e0d3fe436d527a53992d9b34c038f36a3d
Kostas Tzoumpas <kststz@gmail.com>
2024-11-19 02:44:12 +0200

    ElrondDapp

--- src/hoc/withElronDapp.tsx
+++ src/hoc/withElronDapp.tsx
@@ -1,42 +1,11 @@
 /* eslint-disable react/display-name */
-
-import dynamic from "next/dynamic";
-
-import { EnvironmentsEnum } from "@multiversx/sdk-dapp/types";
 import { AxiosInterceptorContext } from "@multiversx/sdk-dapp/wrappers/AxiosInterceptorContext";
-import { Box, Flex } from "@chakra-ui/react";
 import { network } from "api/net.config";
 import { sampleAuthenticatedDomains } from "../config";
-import classNames from "classnames";
-
-export const DappProvider = dynamic(
-  async () => {
-    return (await import('@multiversx/sdk-dapp/wrappers/DappProvider')).DappProvider;
-  },
-  { ssr: false }
-);
-
-const SignTransactionsModals: any = dynamic(
-  async () => {
-    return (await import("@multiversx/sdk-dapp/UI/SignTransactionsModals"))
-      .SignTransactionsModals;
-  },
-  { ssr: false }
-);
-const NotificationModal: any = dynamic(
-  async () => {
-    return (await import("@multiversx/sdk-dapp/UI/NotificationModal"))
-      .NotificationModal;
-  },
-  { ssr: false }
-);
-const TransactionsToastList: any = dynamic(
-  async () => {
-    return (await import("@multiversx/sdk-dapp/UI/TransactionsToastList"))
-      .TransactionsToastList;
-  },
-  { ssr: false }
-);
+import { DappProvider } from "@multiversx/sdk-dapp/wrappers/DappProvider/DappProvider";
+import { TransactionsToastList } from "@multiversx/sdk-dapp/UI/TransactionsToastList";
+import { NotificationModal } from "@multiversx/sdk-dapp/UI/NotificationModal";
+import { SignTransactionsModals } from "@multiversx/sdk-dapp/UI/SignTransactionsModals";
 
 const withElronDapp = (Component) => (props) => {
   return ( 
@@ -53,13 +22,12 @@ const withElronDapp = (Component) => (props) => {
               walletConnectV2ProjectId: "6bec64742092caa5f1b382312bb17be2",
             }}
             dappConfig={{
+              isSSR: true,
               shouldUseWebViewProvider: true,
             }}
           >
             <AxiosInterceptorContext.Listener>
               <TransactionsToastList
-                // customToastClassName="transactions-toast-class"
-                // transactionToastClassName="transactions-toast-class"
                 successfulToastLifetime={6000}
               />
               <NotificationModal/>
