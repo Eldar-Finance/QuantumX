db410943376afdd43425d1f8b19ba11721684bc5
klepi21 <kolepidas@gmail.com>
2024-12-13 23:08:00 +0200

    wl

--- src/views/Burnium/Burnium.tsx
+++ src/views/Burnium/Burnium.tsx
@@ -40,7 +40,8 @@ const whitelistedAddresses = [
   "erd13ers9u3ts4lxa9zgdafptdd4735yjw6wznt2djm4rr4wm0hdu0eqdu69j2",
   "erd17u9myqadv7vvqswj4zvs3hdnzgc28wvx8rp0k7cm9qz98tqv4cusq6sry8",
   "erd10znpcqha0kmqtzas3fkyvgzs8ks03runhy64vkm83dgu0acgyp4sgy534e",
-  "erd1gxlahjmplf7h2w27cwzkx0j4tp04lyd9avgnxhajzfssdc8mr8lqvncrc2"
+  "erd1gxlahjmplf7h2w27cwzkx0j4tp04lyd9avgnxhajzfssdc8mr8lqvncrc2",
+  "erd1uampleqer7jn9a9ysd63xpt8427zetrse4vrf3w4s8hskv2kd45qs94tx7"
 ];
 
 const FeatureCard = ({ title, description, icon, cardBg, textColor, descriptionColor, accentColor }) => (
