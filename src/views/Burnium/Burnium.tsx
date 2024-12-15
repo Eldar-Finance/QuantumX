2375aa0ce94cfdf493cc5451817ce0c032365841
klepi21 <kolepidas@gmail.com>
2024-12-15 21:41:05 +0200

    Update Burnium.tsx

--- src/views/Burnium/Burnium.tsx
+++ src/views/Burnium/Burnium.tsx
@@ -41,7 +41,7 @@ const whitelistedAddresses = [
   "erd17u9myqadv7vvqswj4zvs3hdnzgc28wvx8rp0k7cm9qz98tqv4cusq6sry8",
   "erd10znpcqha0kmqtzas3fkyvgzs8ks03runhy64vkm83dgu0acgyp4sgy534e",
   "erd1gxlahjmplf7h2w27cwzkx0j4tp04lyd9avgnxhajzfssdc8mr8lqvncrc2",
-  "erd1uampleqer7jn9a9ysd63xpt8427zetrse4vrf3w4s8hskv2kd45qs94tx7"
+  "erd1uampleqer7jn9a9ysd63xpt8427zetrse4vrf3w4s8hskv2kd45qs94tx7",
 ];
 
 const FeatureCard = ({ title, description, icon, cardBg, textColor, descriptionColor, accentColor }) => (
